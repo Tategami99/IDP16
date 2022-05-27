35b72a44bc6ae6e9c1c2d097dae5278d4223e239
SebastianSuarez10 <sebastian.suarez105@gmail.com>
2022-05-27 10:34:21 -0700

    revision 53
    poop

--- Assets/Scripts/CobraScripts/c9key.cs
+++ Assets/Scripts/CobraScripts/c9key.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class c9key : MonoBehaviour
 {
     public static string cobraStatueCode = "";
+    [SerializeField] TMP_Text cobraCase;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +21,7 @@ public class c9key : MonoBehaviour
     void OnMouseDown()
     {
         cobraStatueCode += "9";
+        cobraCase.text += "9";
+
     }
 }
