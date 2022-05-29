d50d5e00fa9ce51d1a9e47fa7c0345252becc265
SebastianSuarez10 <sebastian.suarez105@gmail.com>
2022-05-28 18:06:51 -0700

    Revision 60
    Changed name of dialogue people and dialogue speed

--- Assets/Scripts/PandaScripts/Panda1DialogueUI.cs
+++ Assets/Scripts/PandaScripts/Panda1DialogueUI.cs
@@ -38,7 +38,7 @@ public class Panda1DialogueUI : MonoBehaviour
             PandaContinue.GetComponent<BoxCollider2D>().enabled = false;
             textLabel.text = "";
             SpeakerLabel.text = "";
-            SpeakerLabel.text = Username.username;
+            SpeakerLabel.text = "Hungry Panda";
             GetComponent<Panda1Dialogue>().RunPanda1Dialogue(textToType:Panda1AlternateLine1, textLabel);
             self.GetComponent<BoxCollider2D>().enabled = false;
             background.sprite = pandawbamboo;
